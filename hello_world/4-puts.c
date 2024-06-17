7a4047a5f56a6908e26dc0a27087f176a6a4da03
duupain <>
2024-06-17 08:58:26 +0000

    c program betty style correction

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 /**
  * main - Entry point
- * 
  * Description: 'prints exactly Programming is like building a multilingual puzzle, followed by a new line.'
- * @puts: write a line or string to the output( stdout ) stream 
- *
+ * @puts: write a line or string to the output( stdout ) stream
  * Return: Always 0 (Success)
  */
 int main(void)
