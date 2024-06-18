8ec0a8a1d2c9021bf99c35c9a3cc38815051c2d9
duupain <>
2024-06-18 09:22:24 +0000

    P2 T5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* main - main block*/
+/* Description: Print all possible combinations of single-digit numbers.*/
+int main(void)
+{
+int x = 0;
+
+while (x < 10)
+{
+printf("%d", x);
+if (x < 9)
+{
+printf(", ");
+}
+x++;
+}
+printf("\n");
+
+return (0);
+}
