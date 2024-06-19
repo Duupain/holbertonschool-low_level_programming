4cb89f9e1dd5ff2ff9055f5f537746e15549e3ff
duupain <>
2024-06-19 09:02:11 +0000

    P2T9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+/*declare a variable*/
+char number;
+for (number = 48; number <= 57 ; number++)
+{
+/*print the value of the variable*/
+putchar(number);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+
+return (0);
+}
