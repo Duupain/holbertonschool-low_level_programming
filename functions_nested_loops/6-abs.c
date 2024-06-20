41d11708a340ffef0d14e53fb3f2bf9b69effef1
duupain <>
2024-06-20 13:35:17 +0000

    P3T6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,1 +1,19 @@
 #include "main.h"
+/**
+*_abs - returns the absolute value of an integer
+* Return: x or resultat
+*@x: number
+*/
+int _abs(int x)
+{
+int resultat;
+if (x < 0)
+{
+resultat = (x * -1);
+return (resultat);
+}
+else
+{
+return (x);
+}
+}
