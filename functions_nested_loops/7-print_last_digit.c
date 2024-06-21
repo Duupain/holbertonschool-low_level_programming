4df73d494b5f1e3891bc60ae1e4d226d966408f5
duupain <>
2024-06-21 07:16:16 +0000

    3T7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,21 @@
 #include "main.h"
 /**
-*_abs - returns the absolute value of an integer
+* print_last_digit - print the last digit of a number
 * Return: x or resultat
 *@x: number
 */
 int print_last_digit(int x)
 {
-  int result;
-  if (x < 0)
-    result = (x % 10);
-      return (result);
+int result;
+if (x >= 0)
+{
+result = (x % 10);
+_putchar(result + '0');
+}
+else if (x < 0)
+{
+result = (-x % 10);
+_putchar(result + '0');
+}
+return (result);
 }
