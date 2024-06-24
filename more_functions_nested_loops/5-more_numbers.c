c17475793c5691f731ea24996986e7e5d54e42a3
duupain <>
2024-06-24 09:21:39 +0000

    P0T5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
-*more_numbers - ecris 0 a 14 sur 10 lignes
-*@n: nombre
-*@l: lignes
+ *more_numbers - ecris 0 a 14 sur 10 lignes
+*n: nombre
+*l: lignes
 */
 void more_numbers(void)
 {
-int n, l;
-for (l = 0; l <= 14; l++)
-{
-for (n = 0; n < 15 ; n++)
-{
-if (n > 9)
-{
-_putchar((n / 10) + '0');
-}
-_putchar((n % 10) + '0');
-}
-_putchar('\n');
-}
+  int n, l;
+  for (l = 0; l < 10; l++)
+    {
+      for (n = 0; n < 15 ; n++)
+	{
+	  if (n > 9)
+	    {
+	      _putchar((n / 10) + '0');
+	    }
+	  _putchar((n % 10) + '0');
+	}
+      _putchar('\n');
+    }
 }
