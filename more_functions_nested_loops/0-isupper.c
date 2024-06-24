9911e01feb03e94d46dc2913a45fc0b154db6d0b
duupain <>
2024-06-24 07:50:26 +0000

    P0 T0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isupper -  checks for uppercase character
+*@c: caractere
+*return: 1 si uppercase , sinon 0
+*/
+int _isupper(int c)
+{
+if ( c >= 65 && c <=90 )
+{
+return (1);
+}
+return (0);
+}
