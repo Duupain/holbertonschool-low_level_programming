98162b91d9a3557282fc15b63ecdb16eeefbad73
duupain <>
2024-06-24 08:42:57 +0000

    P0T4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/**                                                                                                                                                                           *print_most_numbers - ecris les chiffre de 1 a 9 sauf 2 et 4
+/**
+*print_most_numbers - ecris les chiffre de 1 a 9 sauf 2 et 4
 */
 void print_most_numbers(void)
 {
