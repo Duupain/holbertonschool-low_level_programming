af150a1463712da30792f4ae9b230151dba12457
duupain <>
2024-06-24 09:25:29 +0000

    P0T5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,17 @@
 */
 void more_numbers(void)
 {
-  int n, l;
-  for (l = 0; l < 10; l++)
-    {
-      for (n = 0; n < 15 ; n++)
-	{
-	  if (n > 9)
-	    {
-	      _putchar((n / 10) + '0');
-	    }
-	  _putchar((n % 10) + '0');
-	}
-      _putchar('\n');
-    }
+int n, l;
+for (l = 0; l < 10; l++)
+{
+for (n = 0; n < 15 ; n++)
+{
+if (n > 9)
+{
+_putchar((n / 10) + '0');
+}
+_putchar((n % 10) + '0');
+}
+_putchar('\n');
+}
 }
