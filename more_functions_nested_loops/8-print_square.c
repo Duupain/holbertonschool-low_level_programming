6e282b3d9f1fa96312bcb41fb69aeb518a8f2f07
duupain <>
2024-06-24 11:44:34 +0000

    P0T8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_square - draws a square
+*@size: taille
+*/
+void print_square(int size)
+{
+int a, b;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0;  a < size; a++)
+{
+for (b = 0; b < size; b++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
