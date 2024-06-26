817beb4823aaca9dcf654797ade4daa52a514b32
duupain <>
2024-06-26 18:27:53 +0000

    p1t9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *_strcpy - prendre l'index de src pour la copier dans dest
 *@dest:  pointeur de type chaine de character
 *@src: pointeur de type chaine de character
 *Return: dest
 */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
 int index = 0;
 while (src[index])
