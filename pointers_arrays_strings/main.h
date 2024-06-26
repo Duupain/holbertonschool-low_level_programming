d3258ce9fb96a0118cb53c3e095c619c03c1dae3
duupain <>
2024-06-26 18:43:49 +0000

    p1t9

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+int _strlen(const char *str);
 void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
@@ -8,4 +8,4 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, const char *src);
