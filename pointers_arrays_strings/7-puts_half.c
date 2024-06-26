6b422253a7f240b00e8ae05233048b6a95d1d749
duupain <>
2024-06-26 13:00:23 +0000

    P1T7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,23 +6,23 @@
 */
 void puts_half(char *str)
 {
-  int start = 0, length = 0;
-  while (str[length] != 0)
-    {
-      length++;
-      }
-  if (length % 2 == 0)
-    {
-      start = length / 2;
-    }
-  else
-    {
-      start = (length + 1) / 2;
-    }
-  while (start < length)
-    {
-      _putchar(str[start]);
-      start++;
-    }
-  _putchar('\n');
+int start = 0, length = 0;
+while (str[length] != 0)
+{
+length++;
+}
+if (length % 2 == 0)
+{
+start = length / 2;
+}
+else
+{
+start = (length + 1) / 2;
+}
+while (start < length)
+{
+_putchar(str[start]);
+start++;
+}
+_putchar('\n');
 }
