7922e976f9d4b071a99c3f2cefa0d01e1fe077bd
duupain <>
2024-06-28 11:31:43 +0000

    P2T4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*reverse_array - reverses the content of an array of integers
+*@a: string
+*@n: variable
+*/
+void reverse_array(int *a, int n)
+{
+int i, temp;
+while (a[i] != '\0')
+{
+i++;
+n++;
+}
+n--;
+for (i = 0; i < (n + 1) / 2; i++)
+{
+temp = a[i];
+a[i] = a[n - i];
+a[n - i] = temp;
+}
+}
