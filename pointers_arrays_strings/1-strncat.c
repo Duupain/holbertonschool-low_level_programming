4c3e11fdb834e2b8381418a0b5093e63742d270a
duupain <>
2024-06-28 07:43:47 +0000

    P2T1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,29 @@
 #include "main.h"
 /**
-*
-*
+*_strncat - concatenates n de src dans dest
+*@dest: string 1
+*@src: string 2
+*@n: valeur de byte array
+*Return: dest
 */
 char *_strncat(char *dest, char *src, int n)
 {
-  int i = 0;
-  int j = 0;
-  if (n <= 0)
-    {
-      return (dest);
-    }
-  while ( n
-  while (dest[j] != '\0')
-    {
-     j++;
-   }
-      for (i = 0; src[i] != '\0'; i++)
-       {
-	 dest[i + j] = src[i];
-       }
-      dest[i + j] = '\0';
-	    {
-	  return (dest);
-	}
-	}
+int i = 0;
+int j = 0;
+if (n <= 0)
+{
+return (dest);
+}
+while (dest[j] != '\0')
+{
+j++;
+}
+for (i = 0; i < n; i++)
+{
+dest[i + j] = src[i];
+}
+dest[i + j] = '\0';
+{
+return (dest);
+}
+}
