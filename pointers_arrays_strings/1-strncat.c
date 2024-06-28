257453f6a1c5182177a7eac57e94a7821319ad92
duupain <>
2024-06-28 08:19:39 +0000

    P2T1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,6 +10,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 int i = 0;
 int j = 0;
+int l = 0;
 if (n <= 0)
 {
 return (dest);
@@ -18,8 +19,16 @@ while (dest[j] != '\0')
 {
 j++;
 }
+while (src[l] != '\0')
+{
+l++;
+}
 for (i = 0; i < n; i++)
 {
+if (l <= i)
+{
+break;
+}
 dest[i + j] = src[i];
 }
 dest[i + j] = '\0';
