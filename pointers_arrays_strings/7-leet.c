7e027a66867a22a95cb752831985efab06845f38
Duupain <>
2024-06-29 10:33:56 +0000

    p2t7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*leet - encodes a string into 1337
+*s: string
+*Return: r qui a la meme valeur que s
+*/
+char *leet(char *s)
+{
+char *r = s;
+char a[] = { 'a', 'e', 'o', 't', 'l' };
+char n[] = { 4, 3, 0, 7, 1 };
+int i = 0;
+while (*s)
+{
+for (i = 0; i < 5; i++)
+{
+if (*s == a[i] || *s == a[i] - 32)
+*s = n[i] + '0';
+}
+s++;
+}
+return (r);
+}
