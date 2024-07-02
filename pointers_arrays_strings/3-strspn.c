83bd0ae024564f59b4b86ba5e9b6bcfef4ae8283
duupain <>
2024-07-02 09:23:05 +0000

    p0t3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+*_strspn - gets the length of a prefix substring
+*@s: the initial segment to be scanned
+*@accept: the bytes to match in s
+*Return: count
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i = 0;
+unsigned int j = 0;
+unsigned int count = 0;
+unsigned int found;
+for (i = 0; s[i] != '\0'; i++)
+{
+found = 0;
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+found = 1;
+break;
+}
+}
+if (!found)
+{
+break;
+}
+count++;
+}
+return (count);
+}
