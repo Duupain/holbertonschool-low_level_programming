76a6e0d6dec525d5c1553134c9b0ca0cea70aef0
duupain <>
2024-07-02 07:45:08 +0000

    p0t1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_memcpy - copies memory area
+*@n: nombre de byte
+*@src: tableau 1
+*@dest: prend le nombre de byte du tableau 1
+*Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+for (i = 0; i < n; i++)
+{
+dest[n] = src[n];
+}
+return (dest);
+}
