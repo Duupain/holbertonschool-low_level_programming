2e9c3c94b2895751feffdae4edc72955a7baf34c
duupain <>
2024-07-05 11:04:45 +0000

    p2t0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - program that prints its name, followed by a new line
+*@argc: Nombre d'arguments de la ligne de commande
+*@argv: Tableau de chaines contenant les arguments de la ligne de commande
+*Return: 0
+*/
+int main(int argc, char **argv)
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
