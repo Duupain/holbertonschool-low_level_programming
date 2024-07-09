96553cae74ec5c991c4608e3824c6c946209ecff
Ema Decot <emadecot03@gmail.com>
2024-07-09 23:22:12 +0200

    p1t1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Alloue de la mémoire en utilisant malloc.
+ * @b: Taille de la mémoire à allouer.
+ * Return: Pointeur vers la mémoire allouée.
+ * Termine le processus avec le statut 98 si l'allocation échoue.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+  
+}
