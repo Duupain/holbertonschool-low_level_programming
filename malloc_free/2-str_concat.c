ccc5ba7004901e4c03db1fec49e29e67ac81fbf4
Ema Decot <emadecot03@gmail.com>
2024-07-09 11:12:31 +0200

    p0t2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings.
- * @s1: The first string
- * @s2: The second string
+ * str_concat - Concatène deux chaînes de caractères.
+ * @s1: La première chaîne de caractères.
+ * @s2: La deuxième chaîne de caractères.
  *
- * Return: A pointer to a newly allocated space in memory
- *         containing the concatenated string,
- *         or NULL if allocation fails.
+ * Return: Un pointeur vers un espace nouvellement alloué en mémoire
+ *         contenant la chaîne concaténée,
+ *         ou NULL si l'allocation échoue.
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -17,32 +17,34 @@ char *str_concat(char *s1, char *s2)
 	int size1 = 0;
 	int size2 = 0;
 	char *res;
+
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
+
 	while (s1[size1] != '\0')
 		size1++;
 
 	while (s2[size2] != '\0')
 		size2++;
+
 	res = malloc((size1 + size2 + 1) * sizeof(char));
 	if (res == NULL)
 	{
 		return (NULL);
 	}
+
 	for (i = 0; i < size1; i++)
 	{
 		res[i] = s1[i];
 	}
+
 	for (j = 0; j < size2; j++)
 	{
 		res[i + j] = s2[j];
 	}
 	res[i + j] = '\0';
+
 	return (res);
 }
