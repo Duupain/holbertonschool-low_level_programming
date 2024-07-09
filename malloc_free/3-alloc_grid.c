d61ebb716d27dcd37531672c65914ebbf35e51a5
Ema Decot <emadecot03@gmail.com>
2024-07-09 12:20:18 +0200

    p0t3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,1 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Alloue un tableau 2D d'entiers initialisés à 0.
+ * @width: Largeur du tableau.
+ * @height: Hauteur du tableau.
+ *
+ * Return: Un pointeur vers le tableau 2D alloué et initialisé à 0,
+ *         ou NULL en cas d'échec d'allocation.
+ */
 int **alloc_grid(int width, int height)
+{
+  int i, j;
+  int **grid;
+  if (width <= 0)
+  {
+    return (NULL);
+  }
+  if (height <= 0)
+  {
+    return (NULL);
+  }
+  grid = malloc(height * sizeof(int *));
+  if (grid == NULL)
+  {
+    return (NULL);
+  }
+  for (i = 0; i < height; i++)
+  {
+    grid[i] = malloc(width * sizeof(int));
+    if (grid[i] == NULL)
+    {
+      for (j = 0; j < i; j++)
+      {
+        free(grid[j]);
+        free(grid);
+        return NULL;
+      }
+    }
+    for (j = 0; j < width; j++)
+    {
+      grid[i][j] = 0;
+    }
+  }
+  return (grid);
+}
