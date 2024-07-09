11aca205f9c956b80aebf53c5f4d8d69b2106a7b
Ema Decot <emadecot03@gmail.com>
2024-07-09 18:02:16 +0200

    p1t0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Alloue de la mémoire en utilisant malloc.
+ * @b: Taille de la mémoire à allouer.
+ * Return: Pointeur vers la mémoire allouée.
+ * Termine le processus avec le statut 98 si l'allocation échoue.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	if (b <= 0)
+	{
+		exit(98);
+	}
+	ptr = malloc(b * sizeof(char));
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
