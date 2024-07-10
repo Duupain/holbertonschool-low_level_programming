171f018458502c767df98b96377c3e7813ad5d77
Ema Decot <emadecot03@gmail.com>
2024-07-10 10:23:24 +0200

    p1t1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,12 +3,52 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - Alloue de la mémoire en utilisant malloc.
- * @b: Taille de la mémoire à allouer.
- * Return: Pointeur vers la mémoire allouée.
- * Termine le processus avec le statut 98 si l'allocation échoue.
+ * string_nconcat - Concatène deux chaînes de caractères.
+ * @s1: Première chaîne.
+ * @s2: Deuxième chaîne.
+ * @n: Nombre de caractères à copier de s2.
+ * Return: Pointeur vers la nouvelle chaîne concatene ou NULL si échec
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  
+	unsigned int i, j;
+	unsigned int size1 = 0, size2 = 0;
+	char *res;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+	if (n >= size2)
+	{
+		n = size2;
+	}
+	res = malloc(size1 + n + 1);
+	if (res == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size1; i++)
+	{
+		res[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		res[i + j] = s2[j];
+	}
+	res[i + j] = '\0';
+
+	return (res);
 }
