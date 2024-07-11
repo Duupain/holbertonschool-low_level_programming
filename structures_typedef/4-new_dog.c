1385b883b9d1e041e15d75f354042a619b516d5a
Duupain <emadecot03@gmail.com>
2024-07-11 15:45:54 +0200

    p2t4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-#include <string.h>
 
 /**
  *new_dog - Crée un nouveau chien.
@@ -19,7 +18,40 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	w->age = age;
-	w->name = name;
-	w->owner = owner;
+	w->name = _strdup(name);
+	w->owner = _strdup(owner);
 	return (w);
+}
+/**
+ *_strdup - returns a pointer to a newly allocated space in memory,
+ *which contains a copy of the string given as a parameter.
+ *@str: string to duplicate
+ *Return: a pointer to the duplicated string or NULL if str is NULL
+ *or if insufficient memory was available
+ */
+char *_strdup(char *str)
+{
+	char *str2;
+	int size = 0;
+	int i;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	size++;
+	str2 = malloc(size * sizeof(char));
+	if (str2 == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		str2[i] = str[i];
+	}
+	return (str2);
 }
