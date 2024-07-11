c35f48cf2c8f6665dddeeaa1d9a0f4a5523a721a
Duupain <emadecot03@gmail.com>
2024-07-11 15:33:38 +0200

    p2t4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
+#include <string.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ *new_dog - Crée un nouveau chien.
+ *@name: Le nom du chien.
+ *@age: L'âge du chien.
+ *@owner: Le propriétaire du chien.
+ *Return: Un pointeur vers la nouvelle structure dog_t,ou NULL en cas d'échec.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *w;
+	w = malloc(sizeof(dog_t));
+	if (w == NULL)
+	{
+		return (NULL);
+	}
+	w->age = age;
+	w->name = name;
+	w->owner = owner;
+	return (w);
 }
