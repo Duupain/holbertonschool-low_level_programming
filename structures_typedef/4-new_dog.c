3a20c0cd611c233129826459dd4fc7b634b9b29c
Ema Decot <emadecot03@gmail.com>
2024-07-12 09:47:33 +0200

    p2t4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,56 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-/**
- *new_dog - Crée un nouveau chien.
- *@name: Le nom du chien.
- *@age: L'âge du chien.
- *@owner: Le propriétaire du chien.
- *Return: Un pointeur vers la nouvelle structure dog_t,ou NULL en cas d'échec.
- */
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *w;
-	w = malloc(sizeof(dog_t));
-	if (w == NULL)
-	{
-		return (NULL);
-	}
-	w->age = age;
-	w->name = _strdup(name);
-	w->owner = _strdup(owner);
-	if (w->name == NULL)
-	{
-		return (NULL);
-	}
-	if (w->owner == NULL)
-	{
-		return (NULL);
-	}
-	return (w);
-}
 
 /**
- *_strdup - returns a pointer to a newly allocated space in memory,
- *which contains a copy of the string given as a parameter.
- *@str: string to duplicate
- *Return: a pointer to the duplicated string or NULL if str is NULL
- *or if insufficient memory was available
+ * _strdup - Returns a pointer to a newly allocated space in memory,
+ *           which contains a copy of the string given as a parameter.
+ * @str: String to duplicate
+ * Return: A pointer to the duplicated string or NULL if str is NULL
+ *         or if insufficient memory was available
  */
 char *_strdup(char *str)
 {
 	char *str2;
-	int size = 0;
-	int i;
+	int size = 0, i;
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
+
 	while (str[size] != '\0')
 	{
 		size++;
 	}
 	size++;
+
 	str2 = malloc(size * sizeof(char));
 	if (str2 == NULL)
 	{
@@ -61,5 +35,43 @@ char *_strdup(char *str)
 	{
 		str2[i] = str[i];
 	}
+
 	return (str2);
+}
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ * Return: A pointer to the new dog_t structure, or NULL if the function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *w;
+
+	w = malloc(sizeof(dog_t));
+	if (w == NULL)
+	{
+		return (NULL);
+	}
+
+	w->name = _strdup(name);
+	if (w->name == NULL)
+	{
+		free(w);
+		return (NULL);
+	}
+
+	w->owner = _strdup(owner);
+	if (w->owner == NULL)
+	{
+		free(w->name);
+		free(w);
+		return (NULL);
+	}
+
+	w->age = age;
+
+	return (w);
 }
