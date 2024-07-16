f60dfb846dbc2f5ee7aabafcbb3a17dbef73ad3a
Ema Decot <emadecot03@gmail.com>
2024-07-16 22:05:07 +0200

    p1t0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,2 +1,32 @@
-
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - Retourne la somme de tous ses paramètres.
+ * @n: Le nombre de paramètres passés à la fonction.
+ * Return: La somme de tous les paramètres.
+ */
 int sum_them_all(const unsigned int n, ...)
+{
+	/*Déclaration de la liste de paramètres variadiques*/
+	va_list args;
+	/*Variable pour stocker la somme*/
+	int sum = 0;
+	unsigned int i;
+
+	/*Initialisation de la liste de paramètres variadiques*/
+	va_start(args, n);
+	if (n == 0)
+	{
+		return (0);
+	}
+	/*Parcourir tous les paramètres variadiques*/
+	for (i = 0; i < n; i++)
+	{
+		/*Ajouter chaque paramètre à la somme*/
+		sum += va_arg(args, int);
+	}
+	/*Libère la mémoire de la liste variadique*/
+	va_end(args);
+	/*Retourner la somme des paramètres*/
+	return (sum);
+}
