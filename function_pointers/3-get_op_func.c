9148572793e2bb00836d7d8c2f61127842cd6633
Ema Decot <emadecot03@gmail.com>
2024-07-16 10:53:23 +0200

    p0t3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -14,28 +14,28 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-  op_t ops[] = {
-      {"+", op_add},
-      {"-", op_sub},
-      {"*", op_mul},
-      {"/", op_div},
-      {"%", op_mod},
-      {NULL, NULL}};
-  int i = 0;
+	op_t ops[] = {
+			{"+", op_add},
+			{"-", op_sub},
+			{"*", op_mul},
+			{"/", op_div},
+			{"%", op_mod},
+			{NULL, NULL}};
+	int i = 0;
 
-  /**
-   * Boucle pour parcourir le tableau ops
-   * jusqu'à trouver une correspondance ou atteindre la fin
-   * Vérifie également que l'opérateur est un seul caractère
-   */
-  while (ops[i].op != NULL)
-  {
-    if (*(ops[i].op) == *s && s[1] == '\0')
-    {
-      /*Retourne le pointeur vers la fonction associée à l'opérateur*/
-      return (ops[i].f);
-    }
-    i++;
-  }
-  return (NULL);
+	/**
+	 * Boucle pour parcourir le tableau ops
+	 * jusqu'à trouver une correspondance ou atteindre la fin
+	 * Vérifie également que l'opérateur est un seul caractère
+	 */
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+		{
+			/*Retourne le pointeur vers la fonction associée à l'opérateur*/
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
