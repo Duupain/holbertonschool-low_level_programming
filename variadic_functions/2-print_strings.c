bf117fa08c9199bbe6c637ad482cb9bc9a1b3ad2
Ema Decot <emadecot03@gmail.com>
2024-07-17 19:23:53 +0200

    p1t2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - Imprime des chaînes suivies d'une nouvelle ligne.
+ * @separator: La chaîne à imprimer entre les chaînes.
+ * @n: Le nombre de paramètres passés à la fonction.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+	unsigned int i;
+	const char *str;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, const char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		printf("%s", str);
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	printf("\n");
+	va_end(args);
+}
