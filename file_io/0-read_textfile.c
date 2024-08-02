abf63598351564450f885fea15ed00bf74621fd2
Duupain <emadecot03@gmail.com>
2024-08-02 18:14:33 +0200

    add file for t0 file io

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters it should read and print.
+ *
+ * Return: The actual number of letters it could read and print,
+ *         or 0 if the file cannot be opened or read, or if filename is NULL,
+ *         or if write fails or does not write the expected amount of bytes.
+ */
+ssize_t
+read_textfile(const char *filename, size_t letters)
+{
+  int fd;
+  ssize_t bytes_read, bytes_written;
+  char *buffer;
+
+  /* Check if filename is NULL */
+  if (filename == NULL)
+    return (0);
+
+  /* Open the file */
+  fd = open(filename, O_RDONLY);
+  if (fd == -1) // Check if file open failed
+    return (0);
+
+  /* Allocate buffer for reading */
+  buffer = malloc(sizeof(char) * letters);
+  if (buffer == NULL)
+  {
+    close(fd);
+    return (0);
+  }
+  /* Read from the file */
+  bytes_read = read(fd, buffer, letters);
+  if (bytes_read == -1)
+  {
+    free(buffer);
+    close(fd);
+    return (0);
+  }
+  /* Write to standard output */
+  bytes_written = write(_fileno(stdout), buffer, bytes_read);
+  if (bytes_written == -1 || bytes_written != bytes_read)
+  {
+    free(buffer);
+    close(fd);
+    return (0);
+  }
+  /* Clean up */
+  free(buffer);
+  close(fd);
+
+  return (bytes_written);
+}
---
+++ file_io/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+typedef __int64 ssize_t;
+typedef __int64 size_t;
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
