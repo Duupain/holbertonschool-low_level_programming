a782f4124aa9d0f15d71beb193093fd1ccc2fd92
Ema Decot <emadecot03@gmail.com>
2024-08-08 17:25:51 +0200

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,6 +2,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include "main.h"
+
+/**
+ * main - Copie le contenu d'un fichier vers un autre fichier.
+ * @argc: Nombre d'arguments passés au programme.
+ * @argv: Tableau d'arguments passés au programme.
+ *
+ * Return: 0 en cas de succès, ou un code d'erreur en cas d'échec.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		handle_error(98, "Error: Can't read from file", argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close_file(fd_from, argv[1]);
+		handle_error(99, "Error: Can't write to", argv[2]);
+	}
+
+	copy(fd_from, fd_to, argv[1], argv[2]);
+
+	close_file(fd_from, argv[1]);
+	close_file(fd_to, argv[2]);
+
+	return (0);
+}
 
 /**
  * handle_error - Affiche un message d'erreur et quitte le programme.
@@ -32,6 +69,8 @@ void close_file(int fd, const char *filename)
  * @fd_to: Descripteur de fichier destination.
  * @file_from: Nom du fichier source.
  * @file_to: Nom du fichier destination.
+ *
+ * Gère les erreurs de lecture et d'écriture.
  */
 void copy(int fd_from, int fd_to, const char *file_from, const char *file_to)
 {
@@ -51,40 +90,4 @@ void copy(int fd_from, int fd_to, const char *file_from, const char *file_to)
 
 	if (bytes_read == -1)
 		handle_error(98, "Error: Can't read from file", file_from);
-}
-
-/**
- * main - Copie le contenu d'un fichier vers un autre fichier.
- * @argc: Nombre d'arguments passés au programme.
- * @argv: Tableau d'arguments passés au programme.
- *
- * Return: 0 en cas de succès, ou un code d'erreur en cas d'échec
- */
-int main(int argc, char *argv[])
-{
-	int fd_from, fd_to;
-
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		handle_error(98, "Error: Can't read from file", argv[1]);
-
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		close_file(fd_from, argv[1]);
-		handle_error(99, "Error: Can't write to", argv[2]);
-	}
-
-	copy_content(fd_from, fd_to, argv[1], argv[2]);
-
-	close_file(fd_from, argv[1]);
-	close_file(fd_to, argv[2]);
-
-	return (0);
 }
